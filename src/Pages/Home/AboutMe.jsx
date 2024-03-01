10fbb53b588896352134a0d4a35b8e8dbb098464
Arman Babazadeh <armanbabazadeh61@gmail.com>
2024-03-01 00:47:49 -0500

    Portfolio/About Done

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,17 @@
+export default function AboutMe(){
+    return(
+        <section id="AboutMe" className="about--section">
+            <div className="about--section--img">
+                <img src="./img/about-me.png" alt="About Me" />
+            </div>
+            <div className="hero--section--content--box about--section box">
+                <div className="hero--section--content">
+                    <p className="section--title">About </p>
+                    <h1 className="skills-section--heading">About me</h1>
+                    <p className="hero--section-description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto ut veritatis, amet assumenda nobis sed voluptates aliquam neque exercitationem quisquam nulla eligendi, atque laborum. Incidunt quo veniam unde illo reprehenderit?</p>
+                    <p className="hero--section-description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto ut veritatis, amet assumenda nobis sed voluptates aliquam neque exercitationem quisquam nulla eligendi</p>
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,11 +1,16 @@
 import HeroSection from "../HeroSection.jsx";
 import MySkills from "../MySkills.jsx";
+import AboutMe from "../AboutMe.jsx";
+import MyPortfolio from "../MyPortfolio.jsx";
 
 export default function Home () {
     return(
         <>
             <HeroSection />
             <MySkills />
+            <AboutMe />
+            <MyPortfolio />
+
         </>
     );
 }
