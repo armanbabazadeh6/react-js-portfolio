ece1a8174d8d443c0b02c89748684c6278a17059
Arman Babazadeh <armanbabazadeh61@gmail.com>
2024-03-04 11:01:08 -0500

    ContactMe Finished JSX/CSS

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -8,36 +8,41 @@ export default function ContactMe() {
             </div>
             <form className="contact--form--container">
                 <div className="container">
-                    <label htmlFor="first-name" className="contant--label">
+                    <label htmlFor="first-name" className="contact--label">
                         <span className="text-md">First Name</span>
                         <input type="text" className="contact--input text-md" name="first-name" id="first-name" required />
                     </label>
-                    <label htmlFor="last-name" className="contant--label">
+                    <label htmlFor="last-name" className="contact--label">
                         <span className="text-md">Last Name</span>
                         <input type="text" className="contact--input text-md" name="last-name" id="last-name" required />
                     </label>
-                    <label htmlFor="email" className="contant--label">
+                    <label htmlFor="email" className="contact--label">
                         <span className="text-md">Email</span>
                         <input type="email" className="contact--input text-md" name="email" id="email" required />
                     </label>
-                    <label htmlFor="phone-number" className="contant--label">
+                    <label htmlFor="phone-number" className="contact--label">
                         <span className="text-md">Phone Number</span>
                         <input type="number" className="contact--input text-md" name="phone-number" id="phone-number" required />
                     </label>
                 </div>
-                <label htmlFor="choose-topic" className="contant--label">
+                <label htmlFor="choose-topic" className="contact--label">
                         <span className="text-md">Choose a topic</span>
-                        <select id="choose-topic" className="contant--input text-md">
+                        <select id="choose-topic" className="contact--input text-md">
                             <option>Select One...</option>
                             <option>Item 1</option>
                             <option>Item 2</option>
                             <option>Item 3</option>
                         </select>
                     </label>
-                    <label htmlFor="message" className="contant--label">
+                    <label htmlFor="message" className="contact--label">
                         <span className="text-md">Message</span>
-                        <textarea className="contact--input text-md" id="message" />
+                        <textarea className="contact--input text-md" id="message" rows="8" placeholder="Type your message..." />
                     </label>
+                    <label htmlFor="checkbox" className="checkbox--label">
+                        <input type="checkbox" required name="checkbox" id="checkbox"/>
+                        <span className="text-sm">I accept the terms</span>
+                    </label>
+                    <div><button className="btn btn-primary contant--form--btn">Submit</button></div>
             </form>
         </section>
     );
